0d8135a243a726345baa4d8b3355a12d678416a3
Sonia Modi <86385739+soniamodi@users.noreply.github.com>
2023-07-16 13:35:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function changeContent() {
-  const contentDiv = document.getElementById('content');
-  contentDiv.innerHTML = '<p>New content after button click.</p>';
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+
+const content1 = document.getElementById("content1");
+const content2 = document.getElementById("content2");
+const content3 = document.getElementById("content3");
+
+button1.addEventListener("click", function() {
+  showContent(content1);
+});
+
+button2.addEventListener("click", function() {
+  showContent(content2);
+});
+
+button3.addEventListener("click", function() {
+  showContent(content3);
+});
+
+function showContent(contentElement) {
+  const allContent = document.getElementsByClassName("content");
+  for (let i = 0; i < allContent.length; i++) {
+    allContent[i].style.display = "none";
+  }
+
+  contentElement.style.display = "block";
 }
